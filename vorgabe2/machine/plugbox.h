2e602473b11cb54097955bc6eb8c673a6e17e4af
Roho1999 <roho.99@web.de>
2023-11-03 14:26:32 +0100

    add vorgabe2

---
+++ vorgabe2/machine/plugbox.h
@@ -0,0 +1,26 @@
+/*****************************************************************************/
+/* Betriebssysteme                                                           */
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              P L U G B O X                                */
+/*                                                                           */
+/*---------------------------------------------------------------------------*/
+/* Abstraktion einer Interruptvektortabelle. Damit kann man die Adresse der  */
+/* Behandlungsroutine fuer jeden Hardware-, Softwareinterrupt und jede       */
+/* Prozessorexception festlegen.                                             */
+/*****************************************************************************/
+
+#ifndef __Plugbox_include__
+#define __Plugbox_include__
+
+#include "guard/gate.h"
+
+class Plugbox
+ {
+private:
+    Plugbox(const Plugbox &other) = delete; // Verhindere Kopieren
+    Plugbox& operator=(const Plugbox& other) = delete; // Verhindere Kopieren
+/* TODO: Hier muesst ihr selbst Code vervollstaendigen */ 
+ };
+
+#endif
