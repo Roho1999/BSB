3f51d09168c40ddf0f6375a306eb8b40ff178657
JuUnland <juunland@uni-osnabrueck.de>
2023-11-07 15:12:28 +0100

    blabla

--- vorgabe1/main.cc
+++ vorgabe1/main.cc
@@ -1,11 +1,11 @@
 /* $Id$ */
+
 #include <bootloader/bootloader_info.h>
 #include <machine/efifb.h>
 #include <object/fb.h>
 #include <machine/efiscr.h>
 #include <device/efistr.h>
-
-/* TODO: Hier muesst ihr selbst Code vervollstaendigen */
+#include <machine/keyctrl.h>
 
 extern BootloaderInfo *bootloader_info;
 
@@ -15,21 +15,46 @@ int main()
 {
 
  
-  short num = 10;
-  short num1 = 100;
-  short num2 = 500;
-  short num3 = 5000;
+  // short num = 10;
+  // short num1 = 100;
+  // short num2 = 500;
+  // short num3 = 5000;
 
-  kout << "  asdasdasd  " << -5 << " s dsdfsdfsdfsdfsdfsdfsdfsdf" <<endl; 
+  // kout << "  asdasdasd  " << -5 << " s dsdfsdfsdfsdfsdfsdfsdfsdf" << endl; 
+  // kout << "JA!" << endl; 
 
-  for(short i = 0; i < 10; i++)
+  // for(short i = 0; i < 2; i++)
+  // {
+  //   kout << hex << "hex " << num << ' ' << num1 << ' ' << num2 << ' ' << num3 << " i: " << i << endl;// << " bla\nbla" << '\n';
+  //   kout << bin << "bin " << num << ' ' << num1 << ' ' << num2 << ' ' << num3 << " i: " << i << endl;// << " bla\nbla" << '\n';
+  //   kout << dec << "dec " << num << ' ' << num1 << ' ' << num2 << ' ' << num3 << " i: " << i << endl;// << " bla\nbla" << '\n';
+  //   kout << oct << "oct " << num << ' ' << num1 << ' ' << num2 << ' ' << num3 << " i: " << i << " " << endl;// << " bla\nbla" << '\n';
+  // }
+
+  bool is_hit = false;
+
+  Keyboard_Controller contr = Keyboard_Controller();
+
+  Key pressed;
+
+  int a = 0;
+  int i = 0;
+  while (!is_hit)
   {
-    kout << hex << "hex " << num << ' ' << num1 << ' ' << num2 << ' ' << num3 << " i: " << i << "     "<< endl;// << " bla\nbla" << '\n';
-    kout << bin << "bin " << num << ' ' << num1 << ' ' << num2 << ' ' << num3 << " i: " << i << "     "<< endl;// << " bla\nbla" << '\n';
-    kout << dec << "dec " << num << ' ' << num1 << ' ' << num2 << ' ' << num3 << " i: " << i << "     "<< endl;// << " bla\nbla" << '\n';
-    kout << oct << "oct " << num << ' ' << num1 << ' ' << num2 << ' ' << num3 << " i: " << i << "     " << endl;// << " bla\nbla" << '\n';
-  }
+    a = 0;
+    for (int i = 0; i < 1000000; i++)
+    {
+      a++;
+    }
+    i += 1;
+    kout << i << ' ';
 
+    // pressed = contr.key_hit();
+    // kout << pressed.ascii() << 'A';
+    // kout << pressed.scancode();
+  }
+  
+  
 
   
  
